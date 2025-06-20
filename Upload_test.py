f74e301c562256bc64f33aa82fe9e2395562c1b5
Realysh <yoonsh071500@gmail.com>
2025-06-20 11:58:08 +0900

    python: 복합대입연산자

--- Upload_test.py
+++ Upload_test.py
@@ -1,1 +1,17 @@
-print('테스트용 코드입니다.')
+#복합 대입 연산자
+a=10
+print(a)
+a+=10 #20
+print(a)
+a-=10 #10
+print(a)
+a/=10 #1
+print(a)
+a*=10 #10
+print(a)
+a%=3 #1
+print(a)
+a+=1 #2
+print(a)
+a**=2 #4
+print(a)
