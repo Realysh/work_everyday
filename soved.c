eb5ac0281edd855d0f60166fd9dcd7afeba00096
Realysh <yoonsh071500@gmail.com>
2025-06-23 11:54:10 +0900

    todo modify: 1

--- soved.c
+++ soved.c
@@ -4,15 +4,15 @@ int main(){
     char ip[1000002];
     fgets(ip,1000002,stdin);
     char *p=strchr(ip,'\n');
-    *p='\0';
+    if(p!=NULL) *p='\0';
     int i,num=0;
-    for(i=0;ip[i]!='\0' && i<1000002;i++){
+    for(i=0;ip[i]!='\0' && i<=1000002;i++){
         if(i==0&&ip[i]==' '){
             i++;
         }
         if(ip[i]!=' '){
             num++;
-            while(ip[i]!=' '){
+            while(ip[i]!=' '&& ip[i]!='\0'){
                 i++;
             }
         }
