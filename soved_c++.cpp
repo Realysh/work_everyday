4743e88e76283e5d66f6e34c498311ebc5acba53
Realysh <yoonsh071500@gmail.com>
2025-06-24 18:41:49 +0900

    c++ ch2: <string> and use getline

--- soved_c++.cpp
+++ soved_c++.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 
 using std::cout; //define랑 같은 개념이네, cout이라고 된 것들은 다 std::임.
 using namespace std; //이름 뭔지 모르는 것 만나면 std안에서 먼저 찾아봐라.
 int main(){ 
-    char pw[11];
+    string password;
+
     while(true){
-        cout << "암호>>";
-        cin>>pw;
-        
-        if(strcmp(pw,"C++")==0){//서로 값이 같으면?
-            cout<<"성공! 프로그램을 종료합니다.";
+        cout<<"암호 입력:";
+        getline(cin,password);//암호 입력
+        if(password == "C++"){ //입력 문자와 진짜 암호 비교
+            cout<<"성공";
             break;
         }
         else{
-            cout<<"실패\n";
+            cout<<"암호가 틀렸습니다.\n";
+            //아니면 다시 while반복
         }
     }
+    
+    
+
     return 0;
 }
