f6e3e737b75832f570063404825d3a38083497f9
Realysh <yoonsh071500@gmail.com>
2025-06-24 16:52:32 +0900

    c++ ch1: complete

--- soved_c++.cpp
+++ soved_c++.cpp
@@ -4,17 +4,13 @@
 */
 #include <iostream>
 
+double area(int r){
+    return 3.14*r*r;
+}
 int main(){ //한줄 짜리 주석문. min()함수의 리턴타입은 int가 표준.
-
-    std::cout << "hello\n"; //화면에 hello 문자열 출력.
-    //혹은
-    std::cout << "hello" << std::endl;
-    //std::endl; <- 조작자(11장에서 설명)
-    
-    std::cout << "첫번째 맛보기";
-    //<< : 연산자(+) 더하기랑 같음.
-    //return 0; //종료 코드. 부모프로세스에게 종료 이유를 전달함.
-    //컴파일러가 return 0 을 안 써도 default로 0을 리턴한다고 생각
-    std::cout << "\nhello world" << "안녕하세요";
-
+    int n=3;
+    char c='#';
+    std::cout << c <<5.5<<'-'<<n<<"hello"<<true<<std::endl;
+    std::cout << "n+5 = "<<n+5<<'\n';
+    std::cout <<"면적은"<<area(n);
 }
