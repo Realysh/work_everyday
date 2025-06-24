e03af99b79c992f00215ba04317cfe0b29535e5d
Realysh <yoonsh071500@gmail.com>
2025-06-24 18:31:52 +0900

    c++ ch2: password search

--- soved_c++.cpp
+++ soved_c++.cpp
@@ -4,16 +4,18 @@
 using std::cout; //define랑 같은 개념이네, cout이라고 된 것들은 다 std::임.
 using namespace std; //이름 뭔지 모르는 것 만나면 std안에서 먼저 찾아봐라.
 int main(){ 
-   cout<<"이름을 입력>>";
-    char name[11];
-    cin >> name; //cin <- 이놈을 그냥 바구니라고 생각하자.
-    //사이즈 넘어가면 안 뽑힘.
-    //cin >> "마 이 클" 이런 식으로 하면
-    //버퍼에는 "이 클"남고 "마"만 cin으로 들어감.
-    cout<<"이름은"<<name<<"입니다.\n";
-
-    char firstname[11];
-    cin >> firstname;
-    cout<< "성은"<<firstname<<"입니다";
+    char pw[11];
+    while(true){
+        cout << "암호>>";
+        cin>>pw;
+        
+        if(strcmp(pw,"C++")==0){//서로 값이 같으면?
+            cout<<"성공! 프로그램을 종료합니다.";
+            break;
+        }
+        else{
+            cout<<"실패\n";
+        }
+    }
     return 0;
 }
