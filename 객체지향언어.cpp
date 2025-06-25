9945f06a4fdb90e4cf5963049c8721c122936f34
Realysh <yoonsh071500@gmail.com>
2025-06-25 21:06:20 +0900

    c++ ch3 day2: rectangle

--- 객체지향언어.cpp
+++ 객체지향언어.cpp
@@ -1,34 +1,24 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-//클래스 선언부
-class Circle{
-public: //공개한다.
-    int radius;
-    double getArea(); //멤버 함수를 가지고 있음.
-
-
+class Rectangle{
+public:
+    int width;
+    int height;
+    int getArea();
 };
-
-// 클래스 구현부
-double Circle::getArea(){
-    return 3.14*radius*radius;
+int Rectangle::getArea(){
+    return width*height;
 }
- 
 int main(){
-    int n; //정수 타입의 변수 n 생성.
-        //정수를 저장할 공간 할당받고 이 공간을 n이라고 부르겠다.
-    Circle donut; //Circle 타입의 객체 donut 생성
-        //Circle 멤버를 모두 저장할 공간을 할당받고 이 공간(객체)를 donut이라고 명명.
+    Rectangle square;
     
-    //객체 활용
-    donut.radius=1;
-    double area = donut.getArea(); //너 면적이 얼마니? >> 실행하게 함.
-    cout << "donut의 면적은"<<area<<"입니다."<<endl;
+    cout<<"너비>>";
+    cin>>square.width;
+    cout<<"높이";
+    cin>>square.height;
 
-    Circle pizza;
-    pizza.radius=30;
-    area = pizza.getArea();
-    cout<<"donut의 면적은"<<area<<"입니다."<<endl;
+    int area=square.getArea();
+    cout<<"사각형의 면적은 "<<area;
+    return 0;
 }
