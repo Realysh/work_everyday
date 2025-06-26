e4b68fb6fecf1a38e08916caa8add5c49d8cfd63
Realysh <yoonsh071500@gmail.com>
2025-06-26 11:33:13 +0900

    c++ day 4 chp3: #ifndef관련 공부

---
+++ Exp.h
@@ -0,0 +1,16 @@
+//선언부
+class Exp{
+private: //encapsulaion / 생략 가능함.
+    int base; //베이스
+    int exp; //지수 / 공개된 지수가 아님.
+public:
+    Exp(int b,int e); //생성자(1) /prototype(원형) 선언
+    Exp(int b); //생성자(2)
+    Exp();
+    int getValue(); //함수는 return type이 있어야 함.
+    //또한 camel case 해야함.->2번째 첫글자 대문자로 표기
+    int getBase() {return base;} //inline 함수
+    int getExp(){ return exp;}
+    bool equals(Exp b);
+    int calc();
+};
--- 객체지향언어.cpp
+++ 객체지향언어.cpp
@@ -1,43 +1,45 @@
+#include <iostream> //전처리를 지시하는 
 #include <iostream>
 using namespace std; //이름 뭔지 모르는 것 만나면 std안에서 먼저 찾아봐라.
 
-class Exp{
-private: //encapsulaion / 생략 가능함.
-    int base; //베이스
-    int exp; //지수 / 공개된 지수가 아님.
-public:
-    Exp(int b,int e); //생성자(1) /prototype(원형) 선언
-    Exp(int b); //생성자(2)
-    Exp();
-    int getValue(); //함수는 return type이 있어야 함.
-    //또한 camel case 해야함.->2번째 첫글자 대문자로 표기
-    int getBase() {return base;} //inline 함수
-    int getExp(){ return exp;}
-    bool equals(Exp b);
-};
-
+#include "Exp.h" //앞에 #이 있으면 컴파일러 -> 전처리기 불러서 먼저 해석하라고 함.
 //구현부
 Exp::Exp(int b,int e){
     base=b;
     exp=e;
+    calc();
 }
 
 Exp::Exp(int b){
     base=b;
     exp=1;
+    calc();
 }
+
+//혹은
+// Exp::Exp(int b):Exp(b,1){
+// }
+//이렇게 풀어냄
+
 
 Exp::Exp(){
     base=1;
     exp=1;
+    calc();
 }
+//해당 코드는
+// Exp::Exp():Exp(1,1){
+// }
 
 int Exp::getValue(){
+    return calc();
+}
+
+int Exp::calc(){
     int res=1;
     for(int i=0;i<exp;i++){
         res=res*base;
     }
-    return res;
 }
 
 bool Exp::equals(Exp b){
