a84e190c9e3716c846fd9a04892fb0618476a0d6
Realysh <yoonsh071500@gmail.com>
2025-06-26 22:16:49 +0900

    c++ day4: private,public(멤버 변수의 public화)

--- 객체지향언어.cpp
+++ 객체지향언어.cpp
@@ -1,46 +1,27 @@
 #include <iostream>
 using namespace std;
 
-//c++ day3 chp3 : 지역객체와 전역객체의 생성 및 소멸 순서
-
-//클래스 선언부
 class Circle{
+// public:
+    int radius;
 public:
-    int r;
     Circle();
-    Circle(int radius);
-    ~Circle();
-    double area();
+    Circle(int r);
+    double getArea();
 };
-Circle glDonut(1000);
-Circle glPizza(2000);
 Circle::Circle(){
-    r=1;
-    cout<<"반지름"<<r<<"원 생성"<<endl;
+    radius=1;
 }
-
-Circle::Circle(int radius):r(radius){
-    cout<<"반지름"<<r<<"원 생성"<<endl;
+Circle::Circle(int r){
+    radius=r;
 }
-
-Circle::~Circle(){
-    cout<<"반지름"<<r<<"원 소멸"<<endl;
-}
-
-double Circle::area(){
-    return r*r*3.14;
-}
-
-void f(){
-    Circle fDonut(100);
-    Circle fPizza(200);
-    cout<<"---------------\n";
-    //소멸선.
-    //이 객체들은 실행이 끝나면 바로 소ㅕㄹ됨.
+double Circle::getArea(){
+    return radius*radius*3.14;
 }
 int main(){
-    Circle donut; //여기서 이미 Circle클래스 호출해서 
-    //원의 반지름 1 출력됨.
-    Circle pizza(30);
-    f(); //함수 실행하면 끝남.4
+    Circle waffle;
+    waffle.radius=5;
+    
+    double area=waffle.getArea();
+    cout<<"원의 반지름>>"<<waffle.radius<<" 원의 넓이>>"<<area<<endl;
 }
