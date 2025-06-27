f194114798b50795eda6207d6c35cdedac6b13b0
Realysh <yoonsh071500@gmail.com>
2025-06-27 18:15:04 +0900

    day 4: 괄호검사 코드

--- 자료구조.c
+++ 자료구조.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-#define maxStackSize 5 //좌측 변수 내지 상수가 우측의 의미이다.
+#include <stdlib.h>
+#include <string.h>
+#define maxStackSize 100 //좌측 변수 내지 상수가 우측의 의미이다.
 
-typedef int element;
+typedef char element;
 typedef struct{
     int top;
     int data[maxStackSize];
@@ -22,30 +24,37 @@ int pop(Stack *s){
     return s->data[s->top--];
 }
 
-void display(Stack *p){
-    int i;
-    for(i=0;i<=(p->top);i++){
-        printf("%d ",p->data[i]);
+int check_matching(const char *in){
+    Stack s;
+    char ch,open_ch;
+    int i,n=strlen(in); //n=문자열의 길이
+    init_stack(&s); //스택의 초기화
+    for(i=0;i<n;i++){
+        ch=in[i];
+        switch(ch){
+            case'(':case'{':case'[':
+                push(ch,&s);
+                break;
+            case')':case'}':case']':
+                if(s.top==-1) return 0;
+                else{
+                    open_ch=(pop(&s));
+                    if((open_ch=='('&&ch!=')')||
+                    (open_ch)=='{'&&ch!='}'||
+                    (open_ch)=='['&&ch!=']')
+                    return 0;
+                }
+                break;
+        }
     }
-    printf("\n");
+    if(s.top!=-1) return 0;
+    return 1;
 }
-
 int main(){
-    Stack s; //스택 선언
-    init_stack(&s);
-    //스택에 데이터 입력 함수 : push
-    push(10,&s);
-    display(&s);
-    push(20,&s);
-    display(&s);
-    push(30,&s);
-    display(&s);
-
-    printf("pop실행값 = %d\n",pop(&s));
-    display(&s);
-    printf("pop실행값 = %d\n",pop(&s));
-    display(&s);
-    printf("pop실행값 = %d\n",pop(&s));
-    display(&s);
+    char *p="{A[(i+1)]=0;}";
+    if(check_matching(p)==1)
+        printf("%s 괄호 검사 성공\n",p);
+    else
+        printf("%s 괄호검사 실패\n",p);
     return 0;
 }
