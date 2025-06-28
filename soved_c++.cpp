5698729f560c5710d410c706e8db55b3f810bf52
Realysh <yoonsh071500@gmail.com>
2025-06-28 23:32:53 +0900

    Todo: 브론즈1

--- soved_c++.cpp
+++ soved_c++.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
-
+#include <cctype>
 using namespace std;
 
 int main(){
-    int t;
-    cin>>t;
-    
-    char arr[t][t];
-    
+    string a;
+    getline(cin,a);
+    int len=a.size();
+    int i,count=0;
+
+    for(i=0;i<len;i++){
+        if(a[i]==' '||a[i]=='0')
+            continue;
+        else{
+            if(a[i]=='-'){
+                i++;
+                while(isdigit(a[i])) i++;
+            }
+            else{
+                count++;
+                while(isdigit(a[i])) i++;
+            }
+        }
+    }
+    cout<<count;
+    return 0;
 }
