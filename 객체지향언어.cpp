b947317930de16518f369a21f5d7f34027f5c9f8
Realysh <yoonsh071500@gmail.com>
2025-07-01 10:59:03 +0900

    c++ day7: stack for문으로 입력

--- 객체지향언어.cpp
+++ 객체지향언어.cpp
@@ -16,23 +16,29 @@ bool Stack::push(int n){
         return false;
     p[tos]=n;
     tos++;
+    return true;
 }
 bool Stack::pop(int &n){
-    if(tos<=0)
+    if(tos==0) //하고 나서 tos--하고 뽑기 때문에. 밑에 코드를 살펴보면 알 수 있음.
         return false;
     tos--;
     n=p[tos];
+    return true;
 }
 int main()
 {
     Stack s;
     int n;
-    s.push(5);
-    s.push(6);
-    s.pop(n);
-    cout<<n;
-    s.pop(n);
-    cout<<n;
-    s.pop(n);
-    cout<<n;
+    for(int i=0;i<10;i++){
+        if(s.push(i)==1)
+            cout<<"성공"<<endl;
+        else
+            cout<<"full 실패"<<endl;
+    }
+    for(int i=10;i>=0;i--){
+        if(s.pop(n)==1)
+            cout<<"성공 "<<n<<endl;
+        else
+            cout<<"empty 실패"<<endl;
+    }   
 }
