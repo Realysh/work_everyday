04106639f7ec10022a4ebebcda74c1560c34f2d2
Realysh <yoonsh071500@gmail.com>
2025-07-02 16:44:33 +0900

    c++: random한 객체 만들기

--- 객체지향언어.cpp
+++ 객체지향언어.cpp
@@ -1,33 +1,59 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-//------------- #ifndef ADD_H---------
-//프로토타입 선언, 선언부만 먼저 알려주는 것
-int add(int x[],int size);
-int add(int x[],int size,int *y);
+class Random{
+    Random(){}
+public:
+    static void seed(){srand((unsigned)time(0));} //씨드 설정
+    static int nextInt(int min=0,int max=32767); //min과 max 사이의 랜덤 정수 리턴
+    static char nextAlphabet(); //랜덤 알파벳 문자 리턴
+    static double nextDouble(); //0보다 크거나 같고 1보다 적은 랜던 실수 리턴
+};
+
+int Random::nextInt(int min,int max){
+    int w = max-min;
+    int n= rand()%w+min;
+    return n;
+}
+char Random::nextAlphabet(){
+    char c;
+    int rn=rand()%26;
+    int i=rand()%2;
+    if(i==0) //소문자
+        c=rn+'a';
+    else //대문자
+        c=rn+'A';
+    return c;
+}
+
+double Random::nextDouble(){
+    double d=(double)rand();
+    d=d/RAND_MAX;
+    return d;
+}
 
 int main(){
-    int a[]={1,2,3,4,5};
-    int b[]={6,7,8,9,10};    
-    int c=add(a,5);
-    int d=add(a,5,b);
-    cout<<c<<endl;
-    cout<<d<<endl;
-}
-int add(int *arr,int size){
-    int sum=0;
-    for(int i=0;i<size;i++){
-        sum+=arr[i];
+    // Random r;
+    Random::seed(); //class 자체를 호출, 객체를 호출하는 것이 아니라.
+    cout<<"1에서 100까지 랜덤한 정수 10개 출력"<<endl;
+    for(int i=0;i<10;i++){
+        int n=Random::nextInt(50,100);
+        cout<<n<<' ';
     }
-    return sum;
-}
-int add(int x[],int size,int *y=NULL){
-    int sum=0;
-    for(int i=0;i<size;i++){
-        sum+=x[i];
+    cout<<endl<<endl;
+
+    cout<<"알파벳을 랜덤하게 10개 출력"<<endl;
+    for(int i=0;i<10;i++){
+        char c=Random::nextAlphabet();
+        cout<<c<<' ';
     }
-    if(y==NULL) return sum;
-    for(int i=0;i<size;i++){
-        sum=sum+y[i];
+    cout<<endl<<endl;
+    
+    cout<<"랜덤한 실수 10개 출력"<<endl;
+    for(int i=0;i<10;i++){
+        double d=Random::nextDouble();
+        cout<<d<<' ';
     }
-    return sum;
+    cout<<endl;
 }
