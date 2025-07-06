0e9f946cc0995c4dad03ac06581250550159fb17
Realysh <yoonsh071500@gmail.com>
2025-07-06 13:51:40 +0900

    c++ chp7 연습문제

--- c++practice/main.cpp
+++ c++practice/main.cpp
@@ -6,31 +6,19 @@ class Power{
 public:
     Power(int kick=0,int punch=0){this->kick=kick;this->punch=punch;} //
     void show();
-    Power operator +(Power op2);
-    Power &operator +=(Power op2);
-    bool operator ==(Power op2);
-};
- 
-Power& Power::operator +=(Power op2){
-    kick=kick+op2.kick;
-    punch=punch+op2.punch;
-    return *this;
-}
-
-Power Power::operator +(Power op2){
-    Power tmp; //디폴트 매개 변수 덕에 생성 가능.
-
-    tmp.kick = kick + op2.kick;
-    tmp.punch = punch + op2.punch;
+    Power operator +(int n);
     
-    return tmp;
-}
+};
 
-bool Power::operator ==(Power op2){
-    if(kick==op2.kick && punch == op2.punch)
-        return true;
-    else
-        return false;
+Power Power::operator +(int n){
+    
+    // kick = kick + n;
+    // punch = punch + n;
+    // return *this;  이렇게 되면 a 자체가 바뀌기 때문에
+    Power tmp;
+    tmp.kick=this->kick+n;
+    tmp.punch=this->punch+n;
+    return tmp;
 }
 
 void Power::show(){
@@ -38,15 +26,14 @@ void Power::show(){
 }
 
 int main(){
-    Power a(3,5),b(4,6),c;
-    a.show();
+    Power a(3,5),b,c;
+    b=a+2; //a.+(2);
     b.show();
-    c=a+b; //컴파일러는 a.+(b) 이렇게 변형을 하여 확인을 함. a라는 객체에서 +()함수를 부르도록 시도
-    c.show();
-    if(a==b) cout<<"두 파워가 같다"<<endl; //a.==(b);
-    else cout<<"두 파워가 다르다"<<endl;
 
-    c=a+=b; // a.+=(b); //이 객체 내에 이 함수가 있어?
+    c=a+2;
     c.show();
+    a.show();
+    // b=++a;
+    // a++=b;
     return 0;
 }
--- 객체지향언어.cpp
+++ 객체지향언어.cpp
@@ -1,59 +1,93 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <string>
 using namespace std;
-class Random{
-    Random(){}
+
+
+class Matrix{
+    int data[2][2];
 public:
-    static void seed(){srand((unsigned)time(0));} //씨드 설정
-    static int nextInt(int min=0,int max=32767); //min과 max 사이의 랜덤 정수 리턴
-    static char nextAlphabet(); //랜덤 알파벳 문자 리턴
-    static double nextDouble(); //0보다 크거나 같고 1보다 적은 랜던 실수 리턴
+    Matrix();
+    Matrix(int a,int b,int c,int d);
+    Matrix operator+(Matrix b);
+    Matrix& operator+=(Matrix b); //참조에 의한 연산으로 그 자체를 바꿔야 하기 때문에 a=a+b ->
+    bool operator==(Matrix b);
+    void show();
 };
 
-int Random::nextInt(int min,int max){
-    int w = max-min;
-    int n= rand()%w+min;
-    return n;
+bool Matrix::operator==(Matrix b){
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            if(data[i][j]!=b.data[i][j])
+                return false;
+        }
+    }
+    return true;
 }
-char Random::nextAlphabet(){
-    char c;
-    int rn=rand()%26;
-    int i=rand()%2;
-    if(i==0) //소문자
-        c=rn+'a';
-    else //대문자
-        c=rn+'A';
-    return c;
+Matrix& Matrix::operator+=(Matrix b){
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            data[i][j] +=b.data[i][j];  
+            }
+        }
+    return *this;
+}
+Matrix::Matrix(){
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            data[i][j]=0;
+        }
+    }
 }
 
-double Random::nextDouble(){
-    double d=(double)rand();
-    d=d/RAND_MAX;
-    return d;
+Matrix::Matrix(int a,int b,int c,int d){
+    data[0][0]=a;
+    data[0][1]=b;
+    data[1][0]=c;
+    data[1][1]=d;
 }
 
+Matrix Matrix::operator + (Matrix b){
+    Matrix tmp;
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            tmp.data[i][j]=data[i][j]+b.data[i][j];
+        }
+    }
+    return tmp;
+}
+
+void Matrix::show(){
+    cout<<"Matrix = { ";
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            cout<<data[i][j]<<' ';
+        }
+    }
+    cout<<'}'<<endl;
+}
 int main(){
-    // Random r;
-    Random::seed(); //class 자체를 호출, 객체를 호출하는 것이 아니라.
-    cout<<"1에서 100까지 랜덤한 정수 10개 출력"<<endl;
-    for(int i=0;i<10;i++){
-        int n=Random::nextInt(50,100);
-        cout<<n<<' ';
-    }
-    cout<<endl<<endl;
+   Matrix a(1,2,3,4),b(2,3,4,5),c;
+    c=a+b; //연산에 대한 의미를 새 생ㅓ할 수 있음. 연산자 중복(다형성) : 같은 표현이라도 객체에 따라서 다양한 형태를 가질 수 있음
+    //a.+(b)
+    a.show();b.show();c.show();
 
-    cout<<"알파벳을 랜덤하게 10개 출력"<<endl;
-    for(int i=0;i<10;i++){
-        char c=Random::nextAlphabet();
-        cout<<c<<' ';
-    }
-    cout<<endl<<endl;
-    
-    cout<<"랜덤한 실수 10개 출력"<<endl;
-    for(int i=0;i<10;i++){
-        double d=Random::nextDouble();
-        cout<<d<<' ';
-    }
-    cout<<endl;
+    //왜 이렇게 되는지에 대해서 고민을 해야함.
+    //위와 같은 과정이 lib에 저장되어 있어서 되는 거임.
+    string x="abc";
+    string y;
+    //숫자는 더하기인
+    y=x+"de";
+    cout<<y;
+
+    (a+=b)+=Matrix(10,10,10,10);
+    a.show();b.show();c.show();
+    // a.+=(b) 기존 a에 b의 객체를 가진 것으로  a가 초기화 됨.
+
+    if(a==c) //a.==(c)
+        cout<<"같다."<<endl;
+    else
+        cout<<"다르다."<<endl;
+    c=a+2;
+    c.show();
+    //이것도 lib에 만들어져 있음.
 }
