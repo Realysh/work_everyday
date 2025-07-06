51bed89a11d2e797a0a8029aa09e4f5063b68b38
Realysh <yoonsh071500@gmail.com>
2025-07-06 17:31:00 +0900

    c++ chp7: friend와 class를 이용한 연산자 구현

--- 객체지향언어.cpp
+++ 객체지향언어.cpp
@@ -1,93 +1,59 @@
 #include <iostream>
-#include <string>
 using namespace std;
-
-
-class Matrix{
-    int data[2][2];
+class Power{
+    int kick;
+    int punch;
 public:
-    Matrix();
-    Matrix(int a,int b,int c,int d);
-    Matrix operator+(Matrix b);
-    Matrix& operator+=(Matrix b); //참조에 의한 연산으로 그 자체를 바꿔야 하기 때문에 a=a+b ->
-    bool operator==(Matrix b);
+    Power(int kick=0,int punch=0){this->kick=kick;this->punch=punch;} //
     void show();
+    friend Power operator ++(Power &op2,int n);
+    friend Power &operator ++(Power &op2);
+    friend Power operator +(Power op1,Power op2);
+    Power &operator <<(int n);
 };
 
-bool Matrix::operator==(Matrix b){
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
-            if(data[i][j]!=b.data[i][j])
-                return false;
-        }
-    }
-    return true;
-}
-Matrix& Matrix::operator+=(Matrix b){
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
-            data[i][j] +=b.data[i][j];  
-            }
-        }
+Power &Power::operator <<(int n){
+    kick+=n;
+    punch+=n;
     return *this;
 }
-Matrix::Matrix(){
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
-            data[i][j]=0;
-        }
-    }
-}
-
-Matrix::Matrix(int a,int b,int c,int d){
-    data[0][0]=a;
-    data[0][1]=b;
-    data[1][0]=c;
-    data[1][1]=d;
-}
-
-Matrix Matrix::operator + (Matrix b){
-    Matrix tmp;
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
-            tmp.data[i][j]=data[i][j]+b.data[i][j];
-        }
-    }
+Power operator +(Power op1,Power op2){
+    Power tmp;
+    tmp.punch=op1.punch+op2.punch;
+    tmp.kick = op1.kick + op2.kick;
     return tmp;
 }
-
-void Matrix::show(){
-    cout<<"Matrix = { ";
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
-            cout<<data[i][j]<<' ';
-        }
-    }
-    cout<<'}'<<endl;
+Power &operator ++(Power &op2){
+    op2.kick++;
+    op2.punch++;
+    return op2;
 }
+
+Power operator ++(Power &op2,int n){
+    Power tmp = op2;
+    op2.kick++;
+    op2.punch++;
+    return tmp;
+}
+void Power::show(){
+    cout<<"kick="<<kick<<','<<"punch="<<punch<<endl;
+}
+
 int main(){
-   Matrix a(1,2,3,4),b(2,3,4,5),c;
-    c=a+b; //연산에 대한 의미를 새 생ㅓ할 수 있음. 연산자 중복(다형성) : 같은 표현이라도 객체에 따라서 다양한 형태를 가질 수 있음
-    //a.+(b)
-    a.show();b.show();c.show();
-
-    //왜 이렇게 되는지에 대해서 고민을 해야함.
-    //위와 같은 과정이 lib에 저장되어 있어서 되는 거임.
-    string x="abc";
-    string y;
-    //숫자는 더하기인
-    y=x+"de";
-    cout<<y;
-
-    (a+=b)+=Matrix(10,10,10,10);
-    a.show();b.show();c.show();
-    // a.+=(b) 기존 a에 b의 객체를 가진 것으로  a가 초기화 됨.
-
-    if(a==c) //a.==(c)
-        cout<<"같다."<<endl;
-    else
-        cout<<"다르다."<<endl;
-    c=a+2;
+    Power a(3,5),b,c;
+    a.show();
+    b.show();
+    b=a++; //값을 먼저 사용하고 이후 증가.
+    a.show();
+    b.show();
+    b=++a;
+    a.show();
+    b.show();
+    c=a+b;
     c.show();
-    //이것도 lib에 만들어져 있음.
+    cout<<"======A status====="<<endl;
+    a.show();
+    a<<1<<1<<1;
+    a.show();
+
 }
