58651ac0946d58bc95aee57ed70cce4a7ff609ea
Realysh <yoonsh071500@gmail.com>
2025-07-10 10:24:31 +0900

    c++ chp 11 입출력 시스템

--- soved_c++.cpp
+++ soved_c++.cpp
@@ -1,54 +1,28 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 using namespace std;
-
-class Book{
-    string title;
-    int price, pages;
-public:
-    Book(string title="",int price=0,int pages=0){
-        this->title=title; this->price=price;this->pages=pages;
+int main(){
+    int count=0;
+    char c; //읽은 문자만 return 해주게 됨.
+    while(true){
+        cin.get(c);
+        if(cin.eof()) //c를 만나면
+            break;
+        else if(c==' ')
+            count++;
+        else if(c=='\n')
+            break;
     }
-    void show(){
-        cout<<title<<' '<<price<<"원 "<<pages<<"페이지 "<<endl;
+    cout<<count<<endl;
+}
+
+void f(){
+    int c; //char 잡으면 안 됨.
+    int count=0;
+    while((c=cin.get())!=EOF){
+        if(c=='a')
+            count++;
+        else if(c=='\n')
+            break;
     }
-    friend Book& operator +=(Book& b,int price);
-    friend Book& operator -=(Book& b,int price);  
-    friend bool operator ==(Book op1,int price);
-    friend bool operator ==(Book op1,const string title);
-    friend bool operator ==(Book op1,Book op2);
-};
-Book &operator +=(Book& b,int price){ //Book이라는 객체의 공간에 b를 대입한 것이다.-> 참조 리턴
-    b.price+=price;
-    return b;   
-}
-Book& operator -=(Book& b,int price){
-    b.price-=price;
-    return b;
-}  
-bool operator ==(Book op1,int price){
-    if(op1.price==price)
-        return true;
-    else return false;
-}
-
-bool operator ==(Book op1,const string title){
-    if(op1.title==title) return true;
-    else return false;
-}
-
-bool operator ==(Book op1,Book op2){
-    if(op1.price==op2.price) return true;
-    else return false;
-}
-int main() {
-    Book a("명품c++",30000,500),b("미래",30000,500);
-    // a+=500;//책 가격 증가
-    // b-=500;//책 가격 감소
-    a.show();
-    b.show();
-
-    if(a==30000) cout<<"정가 30000원"<<endl;
-    if(a=="명품 c++")cout<<"명품 c++ 입니다."<<endl;
-    if(a==b) cout<<"두 책이 같은 책입니다."<<endl;
+    cout<<count<<endl;
 }
--- 객체지향언어+.cpp
+++ 객체지향언어+.cpp
@@ -1,24 +1,68 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-
-//이 함수 하나를 여러 곳에 적용할 수 있음.
-template <class T> //template 함수
-T biggest(T a[],int size){ //a는 generic인 t type임
-    T big=a[0];
-    for(int i=0;i<size;i++){
-        if(big<a[i])
-            big=a[i];
+class Student{
+public:
+    Student(string name,int score){
+        this->name = name;
+        this->score = score;
     }
-    return big;
+    string name;
+    int score;
+};
+class ScoreManager{
+    vector<Student> list;
+    string name;
+    void input();
+    void search();
+public:
+    ScoreManager(string name){this->name=name;}
+    void run(); //전원만 넣는다.
+};
+
+void ScoreManager::run(){
+    cout<<"***** 점수 관리 프로그램 "<<name<<"을 시작합니다."<<endl;
+    while(true){
+        cout<<"입력:1, 조회:2, 종료:3>>";
+        int menu;
+        cin>>menu;
+        switch(menu){
+            case 1: input(); break;
+            case 2: search(); break;
+            case 3: cout<<"프로그램을 종료합니다..."<<endl; break;
+            default: return;
+        }
+    }
 }
-int main(){
-    int x[]={1,10,100,5,4};
-    cout<<biggest(x,5)<<endl; //5는 배열 크기, 100이 출력됨.
 
-    double d[]={1.5,10.5,100.6,5.7,4.2,200.8,195.7};
-    cout<<biggest(d,7)<<endl;
+void ScoreManager::input(){
+    cout<<"이름과 점수>>";
+    string name;
+    int score;
+    cin>>name>>score;
+    list.push_back(Student(name,score)); //stack처럼 넣는 건가?
+    cout<<list.size()<<"개 저장됨"<<endl;
+}
 
-    string s[]={"hello","good","computer"};
-    cout<<biggest(s,3)<<endl;
+void ScoreManager::search(){
+    cout<<"이름>>";
+    string name;
+    int score;    
+    cin>>name;
+    for(int i=0;i<list.size();i++){
+        Student s=list.at(i);
+        if(s.name==name){ //found;
+            cout<<name<<"의 점수는"<<s.score<<"입니다"<<endl;
+            return;
+        }
+    }
+    cout<<name<<"은 없는 학생입니다."<<endl;
+    return;
+}
+
+int main(){ //메인은 entry 파일 개념임
+    ScoreManager sMan("AI Score");
+    sMan.run();
 }
